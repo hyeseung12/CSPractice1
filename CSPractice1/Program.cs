73d9b0729814c2da22b66d52bfb3903fef1a0d14
MsEmily1020 <peace10200khs@gmail.com>
2024-06-28 11:13:28 +0900

    #4 21-4. 인스턴스 변수

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSPractice1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -165,7 +166,25 @@ namespace CSPratice1
             Console.WriteLine(Math.PI); // 3.141592~~
 
             // #4 21-3. 클래스 생성
-            
+
+            // #4 21-4. 인스턴스 변수
+            User user1 = new User();
+            user1.name = "이미림";
+            user1.phoneNumber = "010-1234-5678";
+
+            Product p1 = new Product();
+            p1.name = "어른폰";
+            p1.price = 99999999;
+
+            Product p2 = new Product()
+            {
+                price = 3000, name = "감자폰"
+            };
+
+            Product p3 = new Product()
+            {
+                name = "공짜폰"
+            };
         }
     }
 }
